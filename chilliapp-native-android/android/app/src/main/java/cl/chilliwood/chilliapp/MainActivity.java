9cc2fe36d1bd15cd31228187847e2b64a4dc989d
chilliwood <alonso.acuna@gmail.com>
2026-08-12 15:33:15 -0400

    Delete chilliapp-native-android directory

--- chilliapp-native-android/android/app/src/main/java/cl/chilliwood/chilliapp/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package cl.chilliwood.chilliapp;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- chilliapp-native-android/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'cl.chilliwood.chilliapp',
-  appName: 'ChilliApp',
-  webDir: 'www'
-};
-
-export default config;
