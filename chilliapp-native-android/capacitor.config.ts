1ebf5dcec236d52dd99efd50a128df38e16a5ebc
chilliwood <alonso.acuna@gmail.com>
2026-08-12 15:27:12 -0400

    Add files via upload

---
+++ chilliapp-native-android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cl.chilliwood.chilliapp',
+  appName: 'ChilliApp',
+  webDir: 'www'
+};
+
+export default config;
